daf990d46135026e3b90bdbbeabcc8f183528640
midhun-v <narenmids@gmail.com>
2021-04-28 10:35:05 +0530

    constrains

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceSix"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceOne")]
+    
+    @IBAction func Roll(_ sender: UIButton) {
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+//      Also can use diceArray.randomElement() to pick randon element from array
     }
-
-
 }
 
